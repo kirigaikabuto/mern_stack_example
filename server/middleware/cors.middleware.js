c48eaa022d5b0afd9aec63de63f71d64b9bb4f3f
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-10-03 00:38:55 +0600

    sdsddds

---
+++ server/middleware/cors.middleware.js
@@ -0,0 +1,8 @@
+function cors(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, DELETE, POST");
+    res.header("Access-Control-Allow-Headers", "Content-type")
+    next();
+}
+
+module.exports = cors
